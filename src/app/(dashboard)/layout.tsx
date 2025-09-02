5b3e016d0b66e4da8b90f88c4dc46f1288d4c15c
tanayvaswani <vaswani.tanay9@gmail.com>
2025-09-03 03:39:32 +0530

    feat: adds sidebar

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,19 @@
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { DashboardSidebar } from "@/modules/dashboard/ui/components/dashboard-sidebar";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: Props) => {
+  return (
+    <SidebarProvider>
+      <DashboardSidebar />
+      <main className="flex flex-col bg-muted h-screen w-screen">
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+};
+
+export default Layout;
---
+++ src/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarSeparator,
+} from "@/components/ui/sidebar";
+import { BotIcon, StarIcon, VideoIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+const firstSection = [
+  {
+    icon: VideoIcon,
+    label: "Meetings",
+    href: "/meetings",
+  },
+  {
+    icon: BotIcon,
+    label: "Agents",
+    href: "/agents",
+  },
+];
+
+const secondSection = [
+  {
+    icon: StarIcon,
+    label: "Upgrade",
+    href: "/upgrade",
+  },
+];
+
+export const DashboardSidebar = () => {
+  return (
+    <Sidebar>
+      <SidebarHeader className="text-sidebar accent-foreground">
+        <Link href={"/"}>
+          <Image src={"/logo.svg"} alt={"logo"} height={36} width={36} />
+          <p className="text-2xl font-semibold">Personae AI</p>
+        </Link>
+
+        <div className="px-4 py-2">
+          <SidebarSeparator className="opacity-10 text-[#5D6B68]" />
+        </div>
+
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {firstSection.map((item) => (
+                  <SidebarMenuItem key={item.href}>
+                    <SidebarMenuButton>
+                      <Link href={item.href}>
+                        <span className="text-sm font-medium tracking-tight">
+                          {item.label}
+                        </span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+      </SidebarHeader>
+    </Sidebar>
+  );
+};
