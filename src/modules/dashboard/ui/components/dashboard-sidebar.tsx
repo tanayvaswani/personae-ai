3a524dbd9140aaacf589e99aeb2a7cbad8ced3b7
tanayvaswani <vaswani.tanay9@gmail.com>
2025-09-11 00:06:45 +0530

    feat: sidebar bottom dropdown

--- src/modules/dashboard/ui/components/dashboard-sidebar.tsx
+++ src/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { cn } from "@/lib/utils";
+import { DashboardUserButton } from "./dashboard-user-button";
 import {
   Sidebar,
   SidebarContent,
@@ -18,7 +19,6 @@ import {
   SidebarMenuItem,
   SidebarSeparator,
 } from "@/components/ui/sidebar";
-import { DashboardUserButton } from "./dashboard-user-button";
 
 const firstSection = [
   {
@@ -54,68 +54,67 @@ export const DashboardSidebar = () => {
         <div className="px-4 py-2">
           <SidebarSeparator className="opacity-10 text-[#5D6B68]" />
         </div>
-        <SidebarContent>
-          <SidebarGroup>
-            <SidebarGroupContent>
-              <SidebarMenu>
-                {firstSection.map((item) => (
-                  <SidebarMenuItem key={item.href}>
-                    <SidebarMenuButton
-                      className={cn(
-                        "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar",
-                        pathname === item.href &&
-                          "bg-linear-to-r/oklch border-[#5D6B68]/10"
-                      )}
-                      isActive={pathname === item.href}
-                      asChild
-                    >
-                      <Link href={item.href}>
-                        <item.icon className="size-5" />
-                        <span className="text-sm font-medium tracking-tight">
-                          {item.label}
-                        </span>
-                      </Link>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                ))}
-              </SidebarMenu>
-            </SidebarGroupContent>
-          </SidebarGroup>
-          <div className="px-4 py-2">
-            <SidebarSeparator className="opacity-10 text-[#5D6B68]" />
-          </div>
-          <SidebarGroup>
-            <SidebarGroupContent>
-              <SidebarMenu>
-                {secondSection.map((item) => (
-                  <SidebarMenuItem key={item.href}>
-                    <SidebarMenuButton
-                      className={cn(
-                        "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar",
-                        pathname === item.href &&
-                          "bg-linear-to-r/oklch border-[#5D6B68]/10"
-                      )}
-                      isActive={pathname === item.href}
-                      asChild
-                    >
-                      <Link href={item.href}>
-                        <item.icon className="size-5" />
-                        <span className="text-sm font-medium tracking-tight">
-                          {item.label}
-                        </span>
-                      </Link>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                ))}
-              </SidebarMenu>
-            </SidebarGroupContent>
-          </SidebarGroup>
-        </SidebarContent>
-
-        <SidebarFooter>
-          <DashboardUserButton />
-        </SidebarFooter>
       </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {firstSection.map((item) => (
+                <SidebarMenuItem key={item.href}>
+                  <SidebarMenuButton
+                    className={cn(
+                      "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar",
+                      pathname === item.href &&
+                        "bg-linear-to-r/oklch border-[#5D6B68]/10"
+                    )}
+                    isActive={pathname === item.href}
+                    asChild
+                  >
+                    <Link href={item.href}>
+                      <item.icon className="size-5" />
+                      <span className="text-sm font-medium tracking-tight">
+                        {item.label}
+                      </span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <div className="px-4 py-2">
+          <SidebarSeparator className="opacity-10 text-[#5D6B68]" />
+        </div>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {secondSection.map((item) => (
+                <SidebarMenuItem key={item.href}>
+                  <SidebarMenuButton
+                    className={cn(
+                      "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar",
+                      pathname === item.href &&
+                        "bg-linear-to-r/oklch border-[#5D6B68]/10"
+                    )}
+                    isActive={pathname === item.href}
+                    asChild
+                  >
+                    <Link href={item.href}>
+                      <item.icon className="size-5" />
+                      <span className="text-sm font-medium tracking-tight">
+                        {item.label}
+                      </span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <DashboardUserButton />
+      </SidebarFooter>
     </Sidebar>
   );
 };
--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -1,4 +1,13 @@
 import { authClient } from "@/lib/auth-client";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 export const DashboardUserButton = () => {
   const { data, isPending } = authClient.useSession();
@@ -7,5 +16,21 @@ export const DashboardUserButton = () => {
     return null;
   }
 
-  return <div></div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-start bg-white/5 hover:bg-white/10 overflow-hidden">
+        {data.user.image ? (
+          <Avatar>
+            <AvatarImage src={data.user.image} />
+          </Avatar>
+        ) : (
+          <Avatar>
+            <AvatarImage src={"/vercel.svg"} />
+          </Avatar>
+        )}
+
+        {data.user.name}
+      </DropdownMenuTrigger>
+    </DropdownMenu>
+  );
 };
