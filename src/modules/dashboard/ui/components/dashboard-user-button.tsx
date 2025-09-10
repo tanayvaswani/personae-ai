aa92d09949e972f4495224a0295ea41f027c2707
tanayvaswani <vaswani.tanay9@gmail.com>
2025-09-10 23:47:03 +0530

    chore: auth check

--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -3,5 +3,9 @@ import { authClient } from "@/lib/auth-client";
 export const DashboardUserButton = () => {
   const { data, isPending } = authClient.useSession();
 
+  if (isPending || !data?.user) {
+    return null;
+  }
+
   return <div></div>;
 };
