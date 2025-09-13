2b18ce9265e76d169078c2a371f608d0978e2b39
tanayvaswani <vaswani.tanay9@gmail.com>
2025-09-14 01:42:02 +0530

    feat: add ErrorBoundary

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,9 +1,14 @@
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { ErrorBoundary } from "react-error-boundary";
 import { Suspense } from "react";
 
 import { getQueryClient, trpc } from "@/trpc/server";
 
-import { AgentsView, AgentsViewLoading } from "@/modules/agents/ui/agents-view";
+import {
+  AgentsView,
+  AgentsViewError,
+  AgentsViewLoading,
+} from "@/modules/agents/ui/agents-view";
 
 const Page = () => {
   const queryClient = getQueryClient();
@@ -12,7 +17,9 @@ const Page = () => {
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <Suspense fallback={<AgentsViewLoading />}>
-        <AgentsView />
+        <ErrorBoundary fallback={<AgentsViewError />}>
+          <AgentsView />
+        </ErrorBoundary>
       </Suspense>
     </HydrationBoundary>
   );
