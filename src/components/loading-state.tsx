e4ae0636f68e9d38e58c5613cfd09112bab93746
tanayvaswani <vaswani.tanay9@gmail.com>
2025-09-14 01:19:04 +0530

    feat: add loading state

---
+++ src/components/loading-state.tsx
@@ -0,0 +1,21 @@
+import { Loader2 } from "lucide-react";
+
+interface Props {
+  title: string;
+  description: string;
+}
+
+export const LoadingState = ({ title, description }: Props) => {
+  return (
+    <div className="py-4 px8 flex flex-1 items-center justify-center">
+      <div className="flex flex-col items-center justify-center gap-y-6 bg-background rounded-l-2xl p-10 shadow-sm">
+        <Loader2 className="size-6 animate-spin text-primary" />
+
+        <div className="flex flex-col gap-y-2 text-center">
+          <h6 className="text-lg font-medium">{title}</h6>
+          <p className="text-sm">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
