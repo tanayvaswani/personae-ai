6c3a9fabf90274b9c93ad450b6397f8c36d9a2cf
tanayvaswani <vaswani.tanay9@gmail.com>
2025-09-14 01:29:29 +0530

    feat: useSuspenseQuery

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,10 +1,20 @@
-import { AgentsView } from "@/modules/agents/ui/agents-view";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+
+import { getQueryClient, trpc } from "@/trpc/server";
+
+import { AgentsView, AgentsViewLoading } from "@/modules/agents/ui/agents-view";
 
 const Page = () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.agents.getMany.queryFilter());
+
   return (
-    <div>
-      <AgentsView />
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<AgentsViewLoading />}>
+        <AgentsView />
+      </Suspense>
+    </HydrationBoundary>
   );
 };
 
