7ec6f98a72908b5425e662551b0d939ec9ec975f
tanayvaswani <vaswani.tanay9@gmail.com>
2025-09-15 23:23:47 +0530

    chore: adds skull ResponsiveDiaglog

---
+++ src/components/responsive-dialog.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useIsMobile } from "@/hooks/use-mobile";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+} from "@/components/ui/dialog";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerDescription,
+} from "@/components/ui/drawer";
+
+interface ResponsiveDialogProps {
+  title: string;
+  description: string;
+  children: React.ReactNode;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export const ResponsiveDiaglog = ({}: ResponsiveDialogProps) => {};
