1415dfea67af0d47529e46d76b82057ee2bcbb68
tanayvaswani <vaswani.tanay9@gmail.com>
2025-09-18 00:06:38 +0530

    feat: form skeleton

--- src/modules/agents/ui/components/agent-form.tsx
+++ src/modules/agents/ui/components/agent-form.tsx
@@ -1,5 +1,45 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { z } from "zod";
+
+import { useTRPC } from "@/trpc/client";
+import { AgentGetOne } from "../../types";
+
+import { agentsInsertSchema } from "../../schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 interface AgentFormProps {
   onSuccess?: () => void;
   onCancel?: () => void;
-  initialValues?: 
+  initialValues?: AgentGetOne;
 }
+
+export const AgentForm = ({
+  onSuccess,
+  onCancel,
+  initialValues,
+}: AgentFormProps) => {
+  const queryClient = useQueryClient();
+  const router = useRouter();
+  const trpc = useTRPC();
+
+  const createAgent = useMutation(
+    trpc.agents.create.mutationOptions({
+      onSuccess: () => {},
+      onError: () => {},
+    })
+  );
+
+  const form = useForm<z.infer<typeof agentsInsertSchema>>({
+    resolver: zodResolver(agentsInsertSchema),
+    defaultValues: {
+      name: "",
+      instructions: "",
+    },
+  });
+
+  return <div className=""></div>;
+};
