a0318ccedf036e9ccd199c66ca03543fe7741c2b
tanayvaswani <vaswani.tanay9@gmail.com>
2025-09-20 14:31:17 +0530

    feat: DataTable Skeleton

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -3,14 +3,41 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
 
 import { useTRPC } from "@/trpc/client";
+import { DataTable } from "../components/data-table";
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
+import { columns, Payment } from "../components/columns";
+
+const mockData: Payment[] = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "728ed52g",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "728ed52h",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+];
 
 export const AgentsView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return (
+    <div>
+      <DataTable data={mockData} columns={columns} />
+    </div>
+  );
 };
 
 export const AgentsViewLoading = () => {
