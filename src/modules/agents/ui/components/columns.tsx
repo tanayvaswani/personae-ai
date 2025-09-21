2755fc6b92b980b99323e8af4e21d47ea33a7087
tanayvaswani <vaswani.tanay9@gmail.com>
2025-09-21 23:23:44 +0530

    feat: adds name accessor key

--- src/modules/agents/ui/components/columns.tsx
+++ src/modules/agents/ui/components/columns.tsx
@@ -1,13 +1,34 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
+import { CornerDownRight } from "lucide-react";
 
 import { AgentGetOne } from "../../types";
+
+import GeneratedAvatar from "@/components/generated-avatar";
 
 export const columns: ColumnDef<AgentGetOne>[] = [
   {
     accessorKey: "name",
     header: "Agent Name",
+    cell: ({ row }) => (
+      <div className="flex flex-col gap-y-1">
+        <div className="flex items-center gap-x-2">
+          <GeneratedAvatar
+            variant={"botttsNeutral"}
+            seed={row.original.name}
+            className="size-6"
+          />
+          <span className="font-semibold capitalize">{row.original.name}</span>
+        </div>
+
+        <div className="flex items-center gap-x-1.5">
+          <CornerDownRight className="size-3 text-muted-foreground" />
+          <span className="text-sm text-muted-foreground capitalize max-w-[200px] truncate">
+            {row.original.instructions}
+          </span>
+        </div>
+      </div>
+    ),
   },
 ];
- 
--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -6,28 +6,7 @@ import { useTRPC } from "@/trpc/client";
 import { DataTable } from "../components/data-table";
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
-import { columns, Payment } from "../components/columns";
-
-const mockData: Payment[] = [
-  {
-    id: "728ed52f",
-    amount: 100,
-    status: "pending",
-    email: "m@example.com",
-  },
-  {
-    id: "728ed52g",
-    amount: 100,
-    status: "pending",
-    email: "m@example.com",
-  },
-  {
-    id: "728ed52h",
-    amount: 100,
-    status: "pending",
-    email: "m@example.com",
-  },
-];
+import { columns } from "../components/columns";
 
 export const AgentsView = () => {
   const trpc = useTRPC();
@@ -35,7 +14,7 @@ export const AgentsView = () => {
 
   return (
     <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-      <DataTable data={mockData} columns={columns} />
+      <DataTable data={data} columns={columns} />
     </div>
   );
 };
