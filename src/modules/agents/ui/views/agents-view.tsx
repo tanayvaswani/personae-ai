4eb79ee567a3c5398c5aaf9ce270e9ff48245ca0
tanayvaswani <vaswani.tanay9@gmail.com>
2025-09-26 00:08:31 +0530

    feat: add row click

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
 import { columns } from "../components/columns";
@@ -14,7 +15,9 @@ import { LoadingState } from "@/components/loading-state";
 export const AgentsView = () => {
   const [filters, setFilters] = useAgentsFilters();
 
+  const router = useRouter();
   const trpc = useTRPC();
+
   const { data } = useSuspenseQuery(
     trpc.agents.getMany.queryOptions({
       ...filters,
@@ -23,7 +26,11 @@ export const AgentsView = () => {
 
   return (
     <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-      <DataTable data={data.items} columns={columns} />
+      <DataTable
+        data={data.items}
+        columns={columns}
+        onRowClick={(row) => router.push(`/agent/${row.id}`)}
+      />
       <DataPagination
         page={filters.page}
         totalPages={data.totalPages}
