bae40a03e536424023682004a2ff871c89bb952d
tanayvaswani <vaswani.tanay9@gmail.com>
2025-10-02 03:53:39 +0530

    chore: meetings and meetingId route

---
+++ src/app/(dashboard)/meetings/[meetingId]/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>Meeting ID Page</div>;
+};
+
+export default Page;
---
+++ src/app/(dashboard)/meetings/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>Meetings Page</div>;
+};
+
+export default Page;
