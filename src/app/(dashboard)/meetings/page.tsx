43cab33bed95e50fb45ec00e9ca41f2badb12abc
tanayvaswani <vaswani.tanay9@gmail.com>
2025-10-02 04:41:21 +0530

    feat: adds prefetching

--- src/app/(dashboard)/meetings/page.tsx
+++ src/app/(dashboard)/meetings/page.tsx
@@ -1,7 +1,23 @@
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+
+import { getQueryClient, trpc } from "@/trpc/server";
 import { MeetingsView } from "@/modules/meetings/ui/views/meetings-view";
 
 const Page = () => {
-  return <MeetingsView />;
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.meetings.getMany.queryOptions({}));
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<div className="">Fallback</div>}>
+        <ErrorBoundary fallback={<div className="">ErrorFallback</div>}>
+          <MeetingsView />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
+  );
 };
 
 export default Page;
