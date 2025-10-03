6d05895acbcc86715df4c4ec396af676e086220d
tanayvaswani <vaswani.tanay9@gmail.com>
2025-10-03 23:49:42 +0530

    feat: adds meetings list header

--- src/modules/meetings/ui/components/meeting-list-header.tsx
+++ src/modules/meetings/ui/components/meeting-list-header.tsx
@@ -11,7 +11,7 @@ export const MeetingsListHeader = () => {
     <>
       <div className="py-4 px-3 md:px-8 flex flex-col gap-y-4">
         <div className="flex items-center justify-between">
-          <h5 className="font-medium text-xl">My Agents</h5>
+          <h5 className="font-medium text-xl">My Meetings</h5>
           <Button onClick={() => setIsDialogOpen(true)}>New Meeting</Button>
         </div>
         <div className="flex items-center gap-x-2 p-1">TODO: FILTERS</div>
