5e1069ecc9b0f3221a75e600332e2fc0a9bbdb80
tanayvaswani <vaswani.tanay9@gmail.com>
2025-10-03 23:52:19 +0530

    feat: add session check

--- src/app/(dashboard)/meetings/page.tsx
+++ src/app/(dashboard)/meetings/page.tsx
@@ -1,7 +1,10 @@
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import { ErrorBoundary } from "react-error-boundary";
 import { Suspense } from "react";
 
+import { auth } from "@/lib/auth";
 import { getQueryClient, trpc } from "@/trpc/server";
 
 import {
@@ -11,7 +14,14 @@ import {
 } from "@/modules/meetings/ui/views/meetings-view";
 import { MeetingsListHeader } from "@/modules/meetings/ui/components/meeting-list-header";
 
-const Page = () => {
+const Page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session) {
+    redirect("/sign-in");
+  }
+
   const queryClient = getQueryClient();
   void queryClient.prefetchQuery(trpc.meetings.getMany.queryOptions({}));
 
