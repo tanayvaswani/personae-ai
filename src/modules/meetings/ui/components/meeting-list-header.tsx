892798bc9795235cec3be34c5e4014f061d8cb41
tanayvaswani <vaswani.tanay9@gmail.com>
2025-10-03 23:49:35 +0530

    feat: adds meetings list header

---
+++ src/modules/meetings/ui/components/meeting-list-header.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+
+export const MeetingsListHeader = () => {
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+
+  return (
+    <>
+      <div className="py-4 px-3 md:px-8 flex flex-col gap-y-4">
+        <div className="flex items-center justify-between">
+          <h5 className="font-medium text-xl">My Agents</h5>
+          <Button onClick={() => setIsDialogOpen(true)}>New Meeting</Button>
+        </div>
+        <div className="flex items-center gap-x-2 p-1">TODO: FILTERS</div>
+      </div>
+    </>
+  );
+};
