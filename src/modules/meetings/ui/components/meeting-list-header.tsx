f5695f382947c2dbb3e96f4480380376a5008930
tanayvaswani <vaswani.tanay9@gmail.com>
2025-10-03 23:54:56 +0530

    feat: adds meetings list header

--- src/modules/meetings/ui/components/meeting-list-header.tsx
+++ src/modules/meetings/ui/components/meeting-list-header.tsx
@@ -3,12 +3,14 @@
 import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
+import { NewMeetingDialog } from "./new-meeting-dialog";
 
 export const MeetingsListHeader = () => {
   const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
 
   return (
     <>
+      <NewMeetingDialog open={isDialogOpen} onOpenChange={setIsDialogOpen} />
       <div className="py-4 px-3 md:px-8 flex flex-col gap-y-4">
         <div className="flex items-center justify-between">
           <h5 className="font-medium text-xl">My Meetings</h5>
---
+++ src/modules/meetings/ui/components/new-meeting-dialog.tsx
@@ -0,0 +1,27 @@
+import { ResponsiveDialog } from "@/components/responsive-dialog";
+
+import { MeetingForm } from "./meeting-form";
+
+interface NewMeetingDialogProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export const NewMeetingDialog = ({
+  open,
+  onOpenChange,
+}: NewMeetingDialogProps) => {
+  return (
+    <ResponsiveDialog
+      title="New Meeting"
+      description="Create New Meeting"
+      open={open}
+      onOpenChange={onOpenChange}
+    >
+      {/* <MeetingForm
+        onSuccess={() => onOpenChange(false)}
+        onCancel={() => onOpenChange(false)}
+      /> */}
+    </ResponsiveDialog>
+  );
+};
